c58bfc2d548987fa36481405cd3f1f7f83181989
betsyvies <ing.betsyvies@gmail.com>
2017-11-13 10:55:33 -0500

    añadiendo interacciones del menu con JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var button = document.getElementById("btn");
+window.onload = function() {
+     var button = document.getElementById("menu");
+     button.addEventListener('click', showMenu);
+}
+
+function showMenu() {
+    var menu = document.getElementById("opcs-menu");
+
+    /* Permite verificar si contiene la clase que oculta el elemento */
+    if(menu.classList.contains('disabled-menu')) {
+        menu.classList.remove('disabled-menu');
+        menu.classList.add('enabled-menu');
+    }
+    else {
+        menu.classList.remove('enabled-menu');
+        menu.classList.add('disabled-menu');
+    }
+}
